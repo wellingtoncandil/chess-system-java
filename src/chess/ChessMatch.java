3f5318b348a9c9d36ba80122eacfca90f39b6a5b
Wellington Candil <wcandil34@gmail.com>
2021-11-10 13:54:35 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,9 +30,14 @@ public class ChessMatch {
 	    return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(6, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(3, 1));
 		board.placePiece(new King(board, Color.WHITE), new Position(3, 2));
+		placeNewPiece('b', 6, new King(board,Color.BLACK));
+		placeNewPiece('f', 3, new Rook(board, Color.BLACK));
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.ChessException;
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private Character column;
+	private int row;
+	
+	public ChessPosition(Character column, int row) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public Character getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	protected Position toPosition() {
+		return new Position(8-row, column-'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+}
